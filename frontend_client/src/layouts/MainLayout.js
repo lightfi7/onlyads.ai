e25f010adab9ff0101d9fb85da40fcf0705e16a2
Fi <158217226+lightfi7@users.noreply.github.com>
2024-04-06 16:27:47 -0700

    fixed

--- frontend_client/src/layouts/MainLayout.js
+++ frontend_client/src/layouts/MainLayout.js
@@ -4,24 +4,29 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import { Container } from "@mui/material";
 import axios from "../services/axios";
+import { useAuth } from "../hooks/useAuth";
+import { v4 as uuidv4, v5 as uuidv5 } from "uuid";
 
 export default function MainLayout() {
+  const { user, isLoading } = useAuth();
+  console.log(user);
   const addIntercomScript = React.useCallback(() => {
     axios
       .get("/api/setting/intercom/get")
       .then((response) => {
+        const v4Id = uuidv4();
         const script = document.createElement("script");
-        script.src = `/intercom.js?app_id=${response.data.app_id}&name=${response.data.name}&user_id=${response.data.user_id}&email=${response.data.email}`;
+        script.src = `/intercom.js?app_id=${response.data.app_id}&name=${user.userName}&user_id=${v4Id}&email=${user.email}`;
         document.body.appendChild(script);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [user]);
 
   React.useEffect(() => {
-    addIntercomScript();
-  }, [addIntercomScript]);
+    if (!isLoading) addIntercomScript();
+  }, [addIntercomScript, isLoading, user]);
 
   return (
     <div>
