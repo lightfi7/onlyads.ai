ed72b78936f7f2ece0135ac1627c8fa4ff113c4f
Fi <158217226+lightfi7@users.noreply.github.com>
2024-04-18 07:42:23 -0700

    Update ads.controller.js

--- backend/controllers/ads.controller.js
+++ backend/controllers/ads.controller.js
@@ -7,379 +7,45 @@ const Ads = db.ads;
 const Membership = db.membership;
 
 exports.findAll = async (req, res) => {
+  let {
+    type = "youtube",
+    q = "",
+    qt = "keyword",
+    sortBy = "desc",
+    perPage = 12,
+    page = 1,
+    impression = [],
+    likes = [],
+    popularity = [],
+    days = [],
+    languages = [],
+    countries = [],
+    date = [],
+  } = req.body;
   try {
     const expired = req.expired;
-    const membership = req.membership;
-
-    let {
-      type = "youtube",
-      q = "",
-      qt = "keyword",
-      sortBy = "desc",
-      perPage = 12,
-      page = 1,
-      impression = [],
-      likes = [],
-      popularity = [],
-      days = [],
-      languages = [],
-      countries = [],
-      date = [],
-    } = req.body;
-
-    languages = languages.map((l) => l.value);
-    countries = countries.map((c) => c.value);
-
-    let sort_id = -1;
-    if (sortBy == "desc") {
-      sort_id = -1;
-    } else sort_id = 1;
-
-    const queryLowered = q.toLowerCase();
-
-    let publisher;
-    switch (type) {
-      case "youtube":
-        publisher = 7;
-        break;
-      case "tiktok":
-        publisher = 43;
-        break;
-      case "facebook":
-        publisher = 1;
-        break;
-      case "all":
-        publisher = 0;
-        break;
-      case "instagram":
-        publisher = 5;
-        break;
-      case "twitter":
-        publisher = 2;
-        break;
-      case "pinterest":
-        publisher = 4;
-        break;
-      case "admob":
-        publisher = 3;
-        break;
-      case "yahoo":
-        publisher = 6;
-        break;
-      default:
-        publisher = 0;
-        break;
-    }
-
-    let skip = perPage * (page - 1);
-
-    if (expired && req.role == "user") {
-      skip = 0;
-      perPage = 12;
-    }
-
-    const queries = [
-      {
-        $addFields: {
-          impressionp: {
-            $cond: {
-              if: { $regexMatch: { input: "$impression", regex: /K$/i } },
-              then: {
-                $multiply: [
-                  {
-                    $toDouble: { $trim: { input: "$impression", chars: "K " } },
-                  },
-                  1000,
-                ],
-              },
-              else: {
-                $cond: {
-                  if: { $regexMatch: { input: "$impression", regex: /M$/i } },
-                  then: {
-                    $multiply: [
-                      {
-                        $toDouble: {
-                          $trim: { input: "$impression", chars: "M " },
-                        },
-                      },
-                      1000000,
-                    ],
-                  },
-                  else: { $toDouble: "$impression" },
-                },
-              },
-            },
-          },
-        },
-      },
-      {
-        $addFields: {
-          like_countp: {
-            $cond: {
-              if: { $regexMatch: { input: "$like_count", regex: /K$/i } },
-              then: {
-                $multiply: [
-                  {
-                    $toDouble: { $trim: { input: "$like_count", chars: "K " } },
-                  },
-                  1000,
-                ],
-              },
-              else: {
-                $cond: {
-                  if: { $regexMatch: { input: "$like_count", regex: /M$/i } },
-                  then: {
-                    $multiply: [
-                      {
-                        $toDouble: {
-                          $trim: { input: "$like_count", chars: "M " },
-                        },
-                      },
-                      1000000,
-                    ],
-                  },
-                  else: { $toDouble: "$like_count" },
-                },
-              },
-            },
-          },
-        },
-      },
-      {
-        $addFields: {
-          popularityp: {
-            $cond: {
-              if: { $regexMatch: { input: "$heat", regex: /K$/i } },
-              then: {
-                $multiply: [
-                  { $toDouble: { $trim: { input: "$heat", chars: "K " } } },
-                  1000,
-                ],
-              },
-              else: {
-                $cond: {
-                  if: { $regexMatch: { input: "$heat", regex: /M$/i } },
-                  then: {
-                    $multiply: [
-                      { $toDouble: { $trim: { input: "$heat", chars: "M " } } },
-                      1000000,
-                    ],
-                  },
-                  else: { $toDouble: "$heat" },
-                },
-              },
-            },
-          },
-        },
-      },
-      // {
-      //   $addFields: {
-      //     days_countp: {
-      //       $cond: {
-      //         if: { $regexMatch: { input: "$days_count", regex: /K$/i } },
-      //         then: {
-      //           $multiply: [
-      //             {
-      //               $toDouble: { $trim: { input: "$days_count", chars: "K " } },
-      //             },
-      //             1000,
-      //           ],
-      //         },
-      //         else: {
-      //           $cond: {
-      //             if: { $regexMatch: { input: "$days_count", regex: /M$/i } },
-      //             then: {
-      //               $multiply: [
-      //                 {
-      //                   $toDouble: {
-      //                     $trim: { input: "$days_count", chars: "M " },
-      //                   },
-      //                 },
-      //                 1000000,
-      //               ],
-      //             },
-      //             else: { $toDouble: "$days_count" },
-      //           },
-      //         },
-      //       },
-      //     },
-      //   },
-      // },
-      {
-        $addFields: {
-          createdDate: {
-            $dateFromString: {
-              dateString: "$first_seen",
-            },
-          },
-        },
-      },
-      {
-        $sort: {
-          createdDate: sort_id,
-          createdAt: -1,
-        },
-      },
-    ];
-
-    if (qt == "keyword") {
-      queries.push({
-        $match: {
-          title: new RegExp(queryLowered, "i"),
-        },
-      });
-    } else if (qt == "text") {
-      queries.push({
-        $match: {
-          body: new RegExp(queryLowered, "i"),
-        },
-      });
-    } else if (qt == "advertiser") {
-      queries.push({
-        $match: {
-          advertiser_name: new RegExp(queryLowered, "i"),
-        },
-      });
-    } else if (qt == "link") {
-      queries.push({
-        $match: {
-          domain: new RegExp(queryLowered, "i"),
-        },
-      });
-    }
-
-    if (publisher != 0) {
-      queries.push({
-        $match: {
-          platform: publisher,
-        },
-      });
-    }
-
-    if (impression.length > 0) {
-      queries.push({
-        $match: {
-          impressionp: {
-            $gte: impression[0],
-            $lte: impression[1],
-          },
-        },
-      });
-    }
-
-    if (likes.length > 0) {
-      queries.push({
-        $match: {
-          like_countp: {
-            $gte: likes[0],
-            $lte: likes[1],
-          },
-        },
-      });
-    }
-
-    if (popularity.length > 0) {
-      queries.push({
-        $match: {
-          popularityp: {
-            $gte: popularity[0],
-            $lte: popularity[1],
-          },
-        },
-      });
-    }
-
-    if (days.length > 0) {
-      let now = new Date();
-      const daysFrom = new Date(now.setDate(now.getDate() - days[1]));
-      now = new Date();
-      const daysTo = new Date(now.setDate(now.getDate() - days[0]));
-      queries.push({
-        $match: {
-          $expr: {
-            $and: [
-              {
-                $gte: [
-                  { $dateFromString: { dateString: "$first_seen" } },
-                  daysFrom,
-                ],
-              },
-              {
-                $lte: [
-                  { $dateFromString: { dateString: "$first_seen" } },
-                  daysTo,
-                ],
-              },
-            ],
-          },
-        },
-      });
-    }
-
-    if (languages.length > 0) {
-      queries.push({
-        $match: {
-          language: { $in: languages },
-        },
-      });
-    }
-
-    if (countries.length > 0) {
-      queries.push({
-        $match: {
-          countries: { $in: countries },
-        },
-      });
-    }
-    if (date.length > 0) {
-      let startDate = new Date(date[0]);
-      let endDate = new Date(date[1]);
-
-      queries.push({
-        $match: {
-          $expr: {
-            $and: [
-              {
-                $gte: [
-                  { $dateFromString: { dateString: "$first_seen" } },
-                  startDate,
-                ],
-              },
-              {
-                $lte: [
-                  { $dateFromString: { dateString: "$first_seen" } },
-                  endDate,
-                ],
-              },
-            ],
-          },
-        },
-      });
-    }
-
-    queries.push({
-      $facet: {
-        metadata: [{ $count: "total" }],
-        data: [{ $skip: skip || 0 }, { $limit: perPage || 12 }],
-      },
-    });
-
-    const response = await axios.post(`${process.env.EXTRA_DB_API}/ads`, {
-      queries: JSON.stringify(queries),
-    });
-
+    const params = {
+      type,
+      q,
+      qt,
+      sortBy,
+      perPage,
+      page,
+      impression,
+      likes,
+      popularity,
+      days,
+      languages,
+      countries,
+      date,
+      expired,
+      role: req.role,
+    };
+    const response = await axios.post(
+      `${process.env.EXTRA_DB_API}/ads`,
+      params
+    );
     res.status(200).send(response.data);
-
-    // Ads.aggregate(queries)
-    //   .allowDiskUse(true)
-    //   .then((data) => {
-    //     let total = 12;
-    //     if (data[0].metadata.length) total = data[0].metadata[0].total;
-    //     res.status(200).send({
-    //       total,
-    //       ads: data[0].data,
-    //     });
-    //   });
   } catch (err) {
     console.log(err);
     res.status(500).send("Faild to get ads, try again or contact!");
